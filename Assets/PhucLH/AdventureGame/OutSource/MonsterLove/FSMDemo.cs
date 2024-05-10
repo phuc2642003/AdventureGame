6f10850efe417729a8bb3d08f3df8fe79b49a6c0
Le Hong Phuc <phuclhhe176850@fpt.edu.vn>
2024-05-10 18:42:47 +0700

    Change the graphic position of prefab object

---
+++ Assets/PhucLH/AdventureGame/OutSource/MonsterLove/FSMDemo.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterLove.StateMachine;
+
+public class FSMDemo : MonoBehaviour
+{
+    public enum StateDemo
+    {
+        State1,
+        State2,
+        State3
+    }
+
+    private StateMachine<StateDemo> m_fsm;
+
+    private void Awake()
+    {
+        m_fsm = StateMachine<StateDemo>.Initialize(this);
+        m_fsm.ChangeState(StateDemo.State1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_fsm.ChangeState(StateDemo.State2);
+        }    
+    }
+    #region FSM
+    void State1_Enter()
+    {
+        Debug.Log("State 1 enter");
+    }
+    void State1_Update()
+    {
+        Debug.Log("State 1 update");
+    }
+    void State1_FixedUpdate()
+    {
+        Debug.Log("State 1 fixed update");
+    }
+    void State1_Exit()
+    {
+        Debug.Log("State 1 exit");
+    }
+    void State1_Finally()
+    {
+        Debug.Log("State 1 final");
+    }
+
+    void State2_Enter()
+    {
+        Debug.Log("State 2 enter");
+    }
+    void State2_Update()
+    {
+        Debug.Log("State 2 update");
+    }
+    void State2_FixedUpdate()
+    {
+        Debug.Log("State 2 fixed update");
+    }
+    void State2_Exit()
+    {
+        Debug.Log("State 2 exit");
+    }
+    void State2_Finally()
+    {
+        Debug.Log("State 2 final");
+    }
+    #endregion
+}
